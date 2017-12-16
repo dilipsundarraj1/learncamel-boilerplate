c33d71521154e99f4bcb13f6c5b12841cc71f1fc
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-12-16 11:56:48 -0600

    added the source files to the boiler plate repo

---
+++ src/main/java/com/learncamel/routes/SampleCamelRoute.java
@@ -0,0 +1,7 @@
+package com.learncamel.routes;
+
+/**
+ * Created by z001qgd on 12/16/17.
+ */
+public class SampleCamelRoute {
+}
---
+++ src/test/java/com/learncamel/routes/SampleCamelRouteTest.java
@@ -0,0 +1,16 @@
+package com.learncamel.routes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by z001qgd on 12/16/17.
+ */
+public class SampleCamelRouteTest {
+
+    @Test
+    public void sampleTest(){
+        assertTrue(true);
+    }
+}
