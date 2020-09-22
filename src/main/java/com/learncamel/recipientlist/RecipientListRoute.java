a51fea9801eb93dc8395001247a07b84cb52da3e
JamieGoodnight <jamiegoodnight3@gmail.com>
2020-09-22 10:37:38 -0400

    :construction: Beginning recipientlistroute

---
+++ src/main/java/com/learncamel/recipientlist/RecipientListRoute.java
@@ -0,0 +1,9 @@
+package com.learncamel.recipientlist;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class RecipientListRoute extends RouteBuilder {
+    public void configure() throws Exception {
+        
+    }
+}
